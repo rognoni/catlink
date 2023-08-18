45af74cba9f9571fe4523979a008d9d09d1666e4
rognoni <rognoni@users.noreply.github.com>
2023-08-18 17:38:10 +0200

    [TDD] PASS Tests\Unit\LinkServiceTest

--- src/CatLink/app/Services/LinkService.php
+++ src/CatLink/app/Services/LinkService.php
@@ -6,8 +6,18 @@ class LinkService
 {
     public static function normalize_category($category)
     {
-        // TODO...
+        $category = trim($category);
+        $category = trim($category, '/');
+        $category_list = explode('/', $category);
 
-        return $category;
+        $category_path = '';
+        foreach($category_list as $category)
+        {
+            $category = trim($category);
+            $category = preg_replace('/\s+/', ' ', $category);
+            $category_path .= "/$category";
+        }
+
+        return $category_path;
     }
 }
